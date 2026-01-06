c22df17daf17cfba48976963b3d56471b051c905
Alvaro-MV <moralesalvaro97@gmail.com>
2026-01-06 19:53:14 +0100

    Added nginx to the system

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,13 +1,30 @@
 <?php
 
+define('DB_HOST', getenv('MYSQL_HOST') . ':3306');
 define('DB_NAME', getenv('MYSQL_DATABASE'));
 define('DB_USER', getenv('MYSQL_USER'));
 define('DB_PASSWORD', getenv('MYSQL_PASSWORD'));
-define('DB_HOST', getenv('MYSQL_HOST'));
+
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
-define('WP_DEBUG', false);
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', true);
 
 define('FS_METHOD', 'direct');
 
