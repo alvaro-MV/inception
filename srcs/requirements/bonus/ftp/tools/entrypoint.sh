00e3fce04f13082ca03e11db50e71bbc88fa2376
Alvaro-MV <moralesalvaro97@gmail.com>
2026-06-02 20:53:34 +0200

    Problemas señalados por chatgpt sobre la implementación del servidor ftp resueltos a priori.

--- srcs/requirements/bonus/ftp/tools/entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/entrypoint.sh
@@ -1,9 +1,38 @@
+#!/bin/sh
+
 set -e
 
-USER="${FT_USER}"
-USER_PASS_FILE="${FTP_PASSWORD_FILE}"
+USER="${FTP_USER}"
+USER_PASS="$(cat "$FTP_PASSWORD_FILE")"
 
-echo -e "${USER}\\${USER_PASS_FILE}" >> /etc/vsftpd/virtual_users.txt
-/usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
+echo "FTP USER: $USER"
 
-/usr/sbin/vsftpd /etc/vsftpd.conf 
+mkdir -p /etc/vsftpd
+mkdir -p /srv/vsftpd
+
+echo "Generating PAM configuration..."
+
+cat > /etc/pam.d/vsftpd_inception << EOF
+#%PAM-1.0
+auth       required     pam_userdb.so db=/etc/vsftpd/virtual_users
+account    required     pam_userdb.so db=/etc/vsftpd/virtual_users
+EOF
+
+echo "Generating PAM database..."
+
+printf "%s\n%s\n" "$USER" "$USER_PASS" \
+    > /etc/vsftpd/virtual_users.txt
+
+/usr/bin/db5.3_load \
+    -T \
+    -t hash \
+    -f /etc/vsftpd/virtual_users.txt \
+    /etc/vsftpd/virtual_users.db
+
+rm -f /etc/vsftpd/virtual_users.txt
+
+echo "PAM database generated successfully."
+
+echo "Starting vsftpd..."
+
+exec /usr/sbin/vsftpd /etc/vsftpd.conf
